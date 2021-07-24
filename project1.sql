46881e161d4d8f07aeab7aadc4064f76940bd6d8
thephotogenicbug <navinkumarachari@gmail.com>
2021-07-24 21:21:35 +0530

    Push Sql Dump

---
+++ project1.sql
@@ -0,0 +1,173 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 24, 2021 at 05:51 PM
+-- Server version: 10.4.19-MariaDB
+-- PHP Version: 8.0.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `project1`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cart`
+--
+
+CREATE TABLE `cart` (
+  `cartid` int(10) NOT NULL,
+  `pid` int(10) DEFAULT NULL,
+  `qty` int(10) DEFAULT NULL,
+  `vendor` int(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `cart`
+--
+
+INSERT INTO `cart` (`cartid`, `pid`, `qty`, `vendor`) VALUES
+(1, 12, 1, 24),
+(2, 12, 1, 24),
+(3, 12, 1, 24),
+(4, 12, 1, 24),
+(5, 12, 1, 24),
+(6, 12, 1, 24),
+(7, 12, 1, 24),
+(8, 12, 1, 24),
+(9, 12, 1, 24);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `myorder`
+--
+
+CREATE TABLE `myorder` (
+  `orderid` int(10) NOT NULL,
+  `pid` int(10) DEFAULT NULL,
+  `qty` int(10) DEFAULT NULL,
+  `vendor` int(10) DEFAULT NULL,
+  `customername` varchar(100) DEFAULT NULL,
+  `mobile` bigint(12) DEFAULT NULL,
+  `address` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
+
+CREATE TABLE `product` (
+  `pid` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `price` float NOT NULL,
+  `qty` int(11) NOT NULL,
+  `details` text NOT NULL,
+  `photo` text NOT NULL,
+  `vendor` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `product`
+--
+
+INSERT INTO `product` (`pid`, `name`, `price`, `qty`, `details`, `photo`, `vendor`) VALUES
+(12, 'Apple', 200, 50, 'Good Quality of Apples ', 'https://i.ibb.co/Trwmpk3/apple.jpg', 24);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(10) NOT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `password` varchar(50) DEFAULT NULL,
+  `mobile` bigint(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `mobile`) VALUES
+(23, 'Naveen', 'test@gmail.com', '1234', 9008006228),
+(24, 'NaveenKumar', 'naveen@gmail.com', '12345', 9008006228),
+(25, 'Achari', 'achari@gmail.com', '1234', 9008006221);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `cart`
+--
+ALTER TABLE `cart`
+  ADD PRIMARY KEY (`cartid`);
+
+--
+-- Indexes for table `myorder`
+--
+ALTER TABLE `myorder`
+  ADD PRIMARY KEY (`orderid`);
+
+--
+-- Indexes for table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`pid`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `cart`
+--
+ALTER TABLE `cart`
+  MODIFY `cartid` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `myorder`
+--
+ALTER TABLE `myorder`
+  MODIFY `orderid` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `product`
+--
+ALTER TABLE `product`
+  MODIFY `pid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
